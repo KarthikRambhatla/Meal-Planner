e634e2173a7dcfd0201d689e432ddad6cbb0ab68
aaknox <azhya.knox@gmail.com>
2021-12-08 15:40:33 -0600

    start script is now set for ease of use

--- src/main.js
+++ src/main.js
@@ -4,9 +4,16 @@ const BrowserWindow = electron.BrowserWindow;
 const path = require('path');
 const url = require('url');
 let mainWindow;
+
+const startUrl = process.env.ELECTRON_START_URL || url.format({
+    pathname: path.join(__dirname, '/../build/index.html'),
+    protocol: 'file:',
+    slashes: true
+});
+
 function createWindow() {
     mainWindow = new BrowserWindow({width: 4500, height: 3000});
-    mainWindow.loadURL('http://localhost:3000');
+    mainWindow.loadURL(startUrl);
     mainWindow.on('closed', function () {
         mainWindow = null
     })
