dc7c6ea254adfcc54dc55fac6978ff497f05dcb5
aaknox <azhya.knox@gmail.com>
2021-12-17 13:33:50 -0600

    fixed minor rendering issue

--- src/components/NavComponents/RootNavigation.js
+++ src/components/NavComponents/RootNavigation.js
@@ -1,15 +1,18 @@
-import { Navbar, Container, Nav, NavDropdown, Image } from "react-bootstrap";
+import { Navbar, Container, Nav, Image } from "react-bootstrap";
 import * as React from 'react';
-import Tabs from '../../models/Tabs'
+import { Tabs } from '../../models/Tabs'
 
 class RootNavigation extends React.Component {
+  //make constructor with var for focused field
+  
   renderMealTab(focused, tabName){
+    console.log(tabName);
     const imageSrc = focused 
-      ? require(`../assets/images/menu/${tabName}-active.png`).default
-      : require(`../assets/images/menu/${tabName}.png`).default;
+      ? require(`../../assets/images/menu/${tabName}-active.png`).default
+      : require(`../../assets/images/menu/${tabName}.png`).default;
   
       return <Image
-        source={imageSrc}
+        src={imageSrc}
         style={{width: 240, height: 60}}
       />
   }
@@ -20,7 +23,7 @@ class RootNavigation extends React.Component {
         <Container>
             <Nav className="me-auto">
               <Nav.Link href="#breakfast">
-                {this.renderMealTab(Tabs.Breakfast)}
+                {this.renderMealTab(true, Tabs.Breakfast)}
               </Nav.Link>
               <Nav.Link href="#lunch">Lunch</Nav.Link>
               <Nav.Link href="#snack">Snack</Nav.Link>
